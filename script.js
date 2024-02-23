2cc100bd21a8102160b67b22e579885dfb33765c
saidakbar4440 <saidakbar4440@gmail.com>
2024-02-23 19:45:13 +0500

    First

---
+++ script.js
@@ -0,0 +1,9 @@
+var  a=prompt('String typedagi malumot kiriting')
+var b=+prompt('Number typedagi malumot kiriting')
+var  c=prompt('Boolean typedagi malumot kiriting')
+
+alert(`
+Siz kiritkan ${a} ${typeof a}
+Siz kiritkan ${b} ${typeof b}
+Siz kiritkan ${c} ${typeof c}
+`)
